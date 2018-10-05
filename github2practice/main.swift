79f2ff4c6f8eb5eccecf9c815e6fb9983119e821
Jabeen Cheema <jabeencheema@pursuit.org>
2018-10-05 17:08:51 -0400

    not so stressed

---
+++ github2practice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  github2practice
+//
+//  Created by Jabeen's MacBook on 10/5/18.
+//  Copyright © 2018 Jabeen's MacBook. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
